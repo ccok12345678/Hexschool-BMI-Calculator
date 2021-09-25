03e8a05dcaf8d8c38f83e8c0d5588fba814bf02d
Karl Yang <ccok12345678@gmail.com>
2021-09-25 17:19:25 +0800

    added cleanData, moment.js

--- js/all.js
+++ js/all.js
@@ -1,38 +1,57 @@
+// eventListeners
+const form = document.querySelector('.js_calculator');
+form.addEventListener('submit', setData ,false);
+
+const cleaner = document.querySelector('.js_cleaner');
+cleaner.addEventListener('click', (e) => {
+  e.preventDefault();
+  clearData();
+}, false)
+
+// locaStorage
 const oldData = JSON.parse(localStorage.getItem('BMI-data'));
-const  BMI_Data = oldData || [];
+let  BMI_Data = oldData || [];
 
 function setData() {
-  let height = document.querySelector('#userHeight').value;
-  const weight = document.querySelector('#userWeight').value;
-
-  if (height <= 0 || weight <= 0) {
-    alert('請填入正確的身高、體重。')
+  const userHeight = document.querySelector('#userHeight').value;
+  const userWeight = document.querySelector('#userWeight').value;
+  
+  if (userHeight <= 0 || userWeight <= 0) {
+    alert('請填入正確的身高、體重。');
     return;
   }
-
-  const BMI = getBMI(height, weight);
-  setLocalStorage(BMI, height, weight);
+  
+  const BMI = getBMI(userHeight, userWeight);  
+  setLocalStorage(BMI, userHeight, userWeight);
 }
 
-function getBMI(h, w) {
-  h = h / 100;
-  let BMI = w / Math.pow(h, 2);
+function getBMI(height, weight) {
+  height = height / 100;
+  let BMI = weight / Math.pow(height, 2);
   BMI = + (Math.round(BMI + 'e+2') + 'e-2');
-  console.log(BMI); 
   return BMI;
 }
 
 function setLocalStorage(b, h, w) {
+  const d = moment().format('MM-DD-YYYY'); 
   const item = {
     BMI: b,
     height: h,
     weight: w,
+    date: d,
   };
   BMI_Data.push(item);
   const str = JSON.stringify(BMI_Data);
   localStorage.setItem('BMI-data', str);
 }
 
+function clearData() {
+  BMI_Data = [];
+  const str = JSON.stringify(BMI_Data);
+  localStorage.setItem('BMI-data', str);
+  
+  // render function
+}
 
-const form = document.querySelector('.js_calculator');
-form.addEventListener('submit', setData, false);
+// renderfunction
+
