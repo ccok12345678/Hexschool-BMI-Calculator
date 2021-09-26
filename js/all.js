05830c0417f41bd9b8279df3a7923fde1729b646
Karl Yang <ccok12345678@gmail.com>
2021-09-26 23:54:15 +0800

    added renderContent()

--- js/all.js
+++ js/all.js
@@ -8,9 +8,17 @@ cleaner.addEventListener('click', (e) => {
   clearData();
 }, false)
 
+
 // locaStorage
 const oldData = JSON.parse(localStorage.getItem('BMI-data'));
 let  BMI_Data = oldData || [];
+
+// init
+init();
+
+function init() {
+  renderContent();
+}
 
 function setData() {
   const userHeight = document.querySelector('#userHeight').value;
@@ -23,6 +31,8 @@ function setData() {
   
   const BMI = getBMI(userHeight, userWeight);  
   setLocalStorage(BMI, userHeight, userWeight);
+
+  renderContent();
 }
 
 function getBMI(height, weight) {
@@ -32,13 +42,31 @@ function getBMI(height, weight) {
   return BMI;
 }
 
-function setLocalStorage(b, h, w) {
-  const d = moment().format('MM-DD-YYYY'); 
+function BMI_determ(BMI) {
+  if (BMI < 18.5) {
+    return '過輕';    
+  } else if (18.5 <= BMI && BMI <= 25) {
+    return '理想';
+  } else if (25 < BMI && BMI <= 30) {
+    return '過重';
+  } else if (30 < BMI && BMI <= 35) {
+    return '輕度肥胖';
+  } else if (35 < BMI && BMI <= 40) {
+    return '中度肥胖';
+  } else {
+    return '嚴重肥胖';
+  }
+}
+
+function setLocalStorage(userBMI, userHeight, userWeight) {
+  const result = BMI_determ(userBMI);
+  const today = moment().format('MM-DD-YYYY'); 
   const item = {
-    BMI: b,
-    height: h,
-    weight: w,
-    date: d,
+    determ: result,
+    BMI: userBMI,
+    height: userHeight,
+    weight: userWeight,
+    date: today,
   };
   BMI_Data.push(item);
   const str = JSON.stringify(BMI_Data);
@@ -51,7 +79,17 @@ function clearData() {
   localStorage.setItem('BMI-data', str);
   
   // render function
+  renderContent();
 }
 
-// renderfunction
+// render function
+function renderContent() {
+  const table = document.querySelector('.js_result');
+  let str = '';
+  for (let i = 0; i < BMI_Data.length; i++) {
+    str += `<tr><td>${BMI_Data[i].determ}</td><td><span class="small">BMI</span> ${BMI_Data[i].BMI}</td><td><span class="small">weight</span>${BMI_Data[i].weight}kg</td><td><span class="small">height</span>${BMI_Data[i].height}cm</td><td><span class="small">${BMI_Data[i].date}</span></td></tr>`;
+  }
+
+  table.innerHTML = str;
+}
 
